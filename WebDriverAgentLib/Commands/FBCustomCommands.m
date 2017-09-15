eb384fca1365c37241b8e0ebb65f1bd1ddd0ad46
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-09-15 18:26:40 +0200

    Add support for pressing device button (home, volume up/down)

--- WebDriverAgentLib/Commands/FBCustomCommands.m
+++ WebDriverAgentLib/Commands/FBCustomCommands.m
@@ -53,6 +53,7 @@
     [[FBRoute GET:@"/wda/batteryInfo"] respondWithTarget:self action:@selector(handleGetBatteryInfo:)],
     [[FBRoute POST:@"/wda/pressButton"] respondWithTarget:self action:@selector(handlePressButtonCommand:)],
     [[FBRoute POST:@"/wda/keyboard/type"] respondWithTarget:self action:@selector(handleTypeCommand:)],
+    [[FBRoute POST:@"/wda/pressDeviceButton"] respondWithTarget:self action:@selector(handlePressDeviceButtonCommand:)],
   ];
 }
 
@@ -212,4 +213,13 @@
   return FBResponseWithOK();
 }
 
++ (id<FBResponsePayload>)handlePressDeviceButtonCommand:(FBRouteRequest *)request
+{
+  XCUIDeviceButton button = (XCUIDeviceButton)[request.arguments[@"button"] integerValue];
+ 
+  XCUIDevice *device = [XCUIDevice sharedDevice];
+  [device pressButton:button];
+  
+  return FBResponseWithOK();
+}
 @end
