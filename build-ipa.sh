46072fff1f6623372aa756c6c7c41d0ee7c039fd
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-09-03 13:31:16 +0200

    use Xcode 10

--- build-ipa.sh
+++ build-ipa.sh
@@ -20,5 +20,10 @@ xcodebuild \
 mkdir -p ./out/ipa/Payload
 cp -r ./out/Build/Products/Release-iphoneos/WebDriverAgentRunner-Runner.app ./out/ipa/Payload/
 cd ./out/ipa
+
+# Dump basic information about the verison of XCTest embedded in this ipa
+/usr/libexec/PlistBuddy -c Print Payload/WebDriverAgentRunner-Runner.app/Frameworks/XCTest.framework/version.plist
+/usr/libexec/PlistBuddy -c Print Payload/WebDriverAgentRunner-Runner.app/Frameworks/XCTest.framework/Info.plist
+
 zip -r ../WebDriverAgent-$TRAVIS_BUILD_NUMBER.zip .
 cd ../../
