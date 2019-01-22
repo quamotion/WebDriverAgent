53d12d603d65274f996639d1369ead74c8641ceb
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-01-22 19:33:32 +0100

    Update code signing data

--- signing/add-key.sh
+++ signing/add-key.sh
@@ -12,6 +12,10 @@ security unlock-keychain -p travis ios-build.keychain
 # Set the keychain timeout to 1 hour (for long builds)
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
+# Decrypt the certificate and provisioning profile
+openssl aes-256-cbc -k "$KEY_PASSWORD" -in ./signing/adhoc.cer.enc -d -a -out ./signing/adhoc.cer
+openssl aes-256-cbc -k "$KEY_PASSWORD" -in ./signing/adhoc.mobileprovision.enc -d -a -out ./signing/adhoc.mobileprovision
+
 # Add certificates to keychain and allow codesign to access them
 security import ./signing/apple.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin/codesign
 security import ./signing/adhoc.cer -k ~/Library/Keychains/ios-build.keychain -A /usr/bin/codesign
