b690747fe0c54e1ed26c6276e2697321bb714008
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-01-22 19:42:53 +0100

    Use values from environment variables

--- build-ipa.sh
+++ build-ipa.sh
@@ -13,9 +13,9 @@ xcodebuild \
     -configuration Release \
     -derivedDataPath ./out \
     -allowProvisioningUpdates \
-    CODE_SIGN_IDENTITY="iPhone Developer: Frederik Carlier (8T9UKUBGY9)" \
+    CODE_SIGN_IDENTITY="$CODE_SIGN_IDENTITY" \
     CODE_SIGN_STYLE="Manual" \
-    DEVELOPMENT_TEAM="TCDK5ELAH7"
+    DEVELOPMENT_TEAM="$DEVELOPMENT_TEAM"
 
 mkdir -p ./out/ipa/Payload
 cp -r ./out/Build/Products/Release-iphoneos/WebDriverAgentRunner-Runner.app ./out/ipa/Payload/
